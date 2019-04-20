7cbc97b507d239c16175d96baa2a62cb98873ead
ylitvyakov <ylitvyakov@yahoo.com>
2019-04-20 11:09:24 +0300

    Add main file

---
+++ index.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+const path = require('path');
+const requireDir = require('require-dir');
+const log = require('./libs/log')(module);
+const { name, major, minor, patch, codename } = require('./app');
+const Mocha = require('mocha');
+const recursive = require('recursive-readdir');
+
+module.exports = {
+  App: require('./src/App'),
+  tasty: new (require('./src/Tasty')),
+};
+
+if (require.main === module) {
+  const program = require('commander');
+
+  program
+    .version(`${name} ${major}.${minor}.${patch} - ${codename}`, '-v, --version')
+    .option('-d, --dir [path]', 'specify a directory', path.join(process.cwd(), 'test'))
+    .parse(process.argv);
+
+  const { dir } = program;
+
+  log.info(`Test path is ${dir}`);
+
+  const mocha = new Mocha();
+
+  recursive(
+    dir,
+    (err, files) => {
+      files.forEach((file) => mocha.addFile(file))
+      mocha.run();
+    }
+  );
+
+  // const tests = requireDir(dir, {
+  //   recursive: true,
+  //   noCache: true,
+  // });
+
+  log.info('Start testing');
+  log.info('Finish testing.');
+}
